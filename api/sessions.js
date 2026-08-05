d6441776acf27583201c4ab9b743d2b8368ff65b
DINSTORE99 <dinstoree41@gmail.com>
2026-08-06 02:09:06 +0700

    Create sessions.js

---
+++ api/sessions.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+
+  try {
+
+    const response = await fetch(
+      `${process.env.BACKEND_URL}/api/sessions`
+    );
+
+    const data = await response.json();
+
+    res.status(response.status).json(data);
+
+  } catch (e) {
+
+    res.status(500).json({
+      success: false,
+      message: "Backend tidak dapat dihubungi",
+      error: e.message
+    });
+
+  }
+
+}
