842ab5360d46aa0865491c8d03cf4714759fb34f
DINSTORE99 <dinstoree41@gmail.com>
2026-08-06 01:47:31 +0700

    Create status.js

---
+++ api/status.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      `${process.env.BACKEND_URL}/api/status`
+    );
+
+    const data = await response.json();
+
+    res.status(response.status).json(data);
+
+  } catch (e) {
+
+    res.status(500).json({
+      success: false,
+      message: "Backend tidak dapat dihubungi",
+      error: e.message
+    });
+
+  }
+}
